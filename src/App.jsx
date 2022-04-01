39af505d79221ac001d31be0e12128258e20fead
= <=>
2022-04-01 17:28:13 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.scss';
+import Home from './pages/home/Home';
+
+const App = () => {
+  return (
+    <>
+        <Home/>
+    </>
+  )
+}
+
+export default App
